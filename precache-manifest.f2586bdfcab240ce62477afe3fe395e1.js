5cbc2dab1c086c8561791fa8e9038265361ab678
Ulisses <ulisses.sigma@gmail.com>
2020-08-31 09:57:53 -0300

    Updates

---
+++ precache-manifest.f2586bdfcab240ce62477afe3fe395e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2b3c1c662e308e217a239656c0b868e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3187a7ce43327848a842",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "30249c3780a622c6b049",
+    "url": "/static/js/2.f2d81a2c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f2d81a2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3187a7ce43327848a842",
+    "url": "/static/js/main.e6bab1db.chunk.js"
+  },
+  {
+    "revision": "969f5279642ccfedf05b",
+    "url": "/static/js/runtime-main.1370cb6b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
